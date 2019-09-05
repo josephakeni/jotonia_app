6b4ea6fd9d763ec326846690aa35b825d0254cad
joseph <josephakeni@gmail.com>
2019-09-05 20:14:35 +0100

    Refactored

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=> res.send('Hello this is the app to test Kubernetes deployement'));
+
+app.listen(port, () => console.log('This app is listening on port ' + port));
