106010901c1c29a452d116f8cd2a7372f30d2403
josephakeni <josephakeni@gmail.com>
2019-12-31 00:00:08 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=> res.send('Hello this is the app to test Kubernetes deployement'));
+app.get('/', (req, res)=> res.send('<h2>Comming Soon <p>Jotonia Logistics<p><h2>'));
 
 app.listen(port, () => console.log('This app is listening on port ' + port));
